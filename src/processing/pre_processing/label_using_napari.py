f460aa277f6eb7bfc13111d3efa7d89970f3716c
Richie Dadhley <richie.s.dadhley@gmail.com>
2024-11-11 11:08:57 +0000

    Updated widgets

--- src/processing/pre_processing/label_using_napari.py
+++ src/processing/pre_processing/label_using_napari.py
@@ -8,6 +8,42 @@ from magicgui import magicgui
 import pathlib
 
 class GroundTruth():
+    """"
+        A class that will allow us to generate the ground truth vesicle spheres 
+        around hand labelled vesicle centres. 
+
+        Attributes
+        -------------------
+        pos_data (array): 
+            An array labelling the vesicle centres for PC+ vesicles.
+        neg_data (array): 
+            An array labelling the vesicle centres for PC- vesicles.
+        voxel_size (tuple(int)): 
+            Tuple containing the (z,y,x) resolution.
+        offset (tuple(int)): 
+            The offset of the array. Required only for converting 
+            result to zarr data. Default is (0,0,0).
+        background_label (int):
+            The background label value of the array. Required only for 
+            converting result to zarr data. Default is 0. 
+        num_classes (int): 
+            The number of different label options, including background.
+            Required only for converting result to zarr data. Default is
+            3 (PC+, PC-, background)
+        axes (list(str)): 
+            List containing axes order for the data. Required only for converting 
+            result to zarr data. Default is ['z','y','x'].
+        balls (dict): 
+            Dictionary who's key is the diameter of a ball and value is 
+            the corresponding skimage ball (see 'draw_ball' function).
+        min_distance (int): 
+            The minimum distance between two vesicle centre labels to be 
+            considered separate vesicle labels (i.e. independent spheres).
+        kernel_shape (array):
+            The size (in voxels) of the spheres to be drawn.
+        ground_truth (array):
+            The array containing the computed ground truth data.
+    """
      
     def __init__(self,
                  pos_data, 
@@ -19,6 +55,42 @@ class GroundTruth():
                  num_classes = 3, 
                  axes =['z','y','x'], 
                  min_distance = 1):
+        
+        """
+            Initialise a groundtruth object. 
+
+            Parameters
+            -------------------
+            pos_data (array):
+                Array containing hand labelled PC+ vesicle centres.
+            neg_data (array):
+                Array containing hand labelled PC- vesicle centres.
+            vesicle_diameter (int):
+                Requested real world diameter of vesicle spheres -- can be
+                in any unit as long as matches 'resolution'. 
+            resolution (tuple(int)):
+                Resolution of the image -- can be in any unit as long as 
+                matches 'vesicle_diameter'. Order should match 'axes'.
+            offset:
+                The offset of the array. Required only for converting 
+                result to zarr data. Default is (0,0,0).
+            background_label (int):
+                The background label value of the array. Required only for 
+                converting result to zarr data. Default is 0. 
+            num_classes (int): 
+                The number of different label options, including background.
+                Required only for converting result to zarr data. Default is
+                3 (PC+, PC-, background)
+            axes (list(str)): 
+                List containing axes order for the data. Required only for converting 
+                result to zarr data. Default is ['z','y','x'].
+            balls (dict): 
+                Dictionary who's key is the diameter of a ball and value is 
+                the corresponding skimage ball (see 'draw_ball' function).
+            min_distance (int): 
+                The minimum distance between two vesicle centre labels to be 
+                considered separate vesicle labels (i.e. independent spheres).
+        """
 
         self.pos_data = pos_data
         self.neg_data = neg_data
@@ -34,6 +106,20 @@ class GroundTruth():
         self.kernel_shape = np.ceil(diameter/self.voxel_size)
         
     def find_centres(self, data):
+        """
+            Find centres of hand labelled data. Convolves data with a ball 
+            of size kernel_shape and looks for peak_local_max values. 
+
+            Parameters
+            -------------------
+            data (array): 
+                The hand labelled vesicle centre data.
+
+            Returns 
+            -------------------
+            centre_indices (ndarry):
+                Vesicle centre coordinates, as given by skimage.feature.peak_local_max.
+        """
 
         kernel = skimage.morphology.ball(100).astype(np.float64) 
         kernel = skimage.transform.resize(kernel, self.kernel_shape).astype(np.float64)
@@ -53,6 +139,19 @@ class GroundTruth():
         return centre_indices
     
     def draw_ball(self, array, location, label=1):
+        """
+            Draw a ball of shape kernel_shape centred at location inside 
+            array, with value label. The array itself will be edited.
+
+            Parameters 
+            -------------------
+            array:
+                The array in which the ball should be drawn. 
+            location:
+                The indices for the centre of the ball. 
+            label (optional): 
+                The label value the ball should take. Default=1.
+        """
 
         diameter = self.kernel_shape
         diameter = tuple(int(x) for x in diameter)
@@ -98,35 +197,34 @@ class GroundTruth():
         view[sliced_ball] = label
 
     def compute_gt(self):
+        """
+            Computes the ground truth data, using the find_centres and 
+            draw_ball functions. 
+
+            Returns 
+            -------------------
+            self.ground_truth:
+                An array containing the computed ground truth.
+        """
 
         hand_labelled_data = self.pos_data + self.neg_data
 
         self.ground_truth = np.zeros(hand_labelled_data.shape, dtype=np.int64)
 
         vesicle_centres = self.find_centres(data = hand_labelled_data)
-        print(vesicle_centres)
         for i in range (len(vesicle_centres)):
             loc = tuple(vesicle_centres[i])
-            # print(hand_labelled_data[loc])
             self.draw_ball(array=self.ground_truth, location=loc, label=hand_labelled_data[loc])
 
         return self.ground_truth
 
 if __name__ == '__main__':
 
-    raw_data_path = 'data/TIF_data/17_1A/raw_clahe.tif'
-    raw_data = np.array([skimage.io.imread(raw_data_path)])
-
-    # Check for stacks of images
-    if (raw_data.shape[0] == 1):
-        raw_data = raw_data[0,:]
-
+    # Load the napari viewer
     viewer = napari.Viewer()
-    viewer.add_image(data=raw_data, name='Raw')
 
-    pos = viewer.add_labels(data = np.zeros(raw_data.shape, dtype=np.int64), name='pos')
-    neg = viewer.add_labels(data = np.zeros(raw_data.shape, dtype=np.int64), name='neg')
-
+    # Define our magicgui widget buttons
+    
     @magicgui(call_button='Compute GT')
     def get_gt(
             vesicle_diameter = 300,
@@ -134,6 +232,19 @@ if __name__ == '__main__':
             resolution_y=60,
             resolution_x=60, 
             min_distance: int = 1) -> napari.types.LayerDataTuple:
+        """
+            Computes the ground truth labelling layer, from the pos and neg layers.
+            The user sets the vesicle_diameter and the resolutions (in any consistent
+            measurement unit) using the provided boxes. The minimum distance between 
+            two labels to be considered independent vesicles is set using the min_distance
+            input. 
+
+            The ground truth label layer (called 'gt') is then generated by clicking the 
+            "Compute GT" call button. 
+        """
+        
+        pos = viewer.layers['pos']
+        neg = viewer.layers['neg']
         ground_truth = GroundTruth(
                                 pos_data = pos.data, 
                                 neg_data = neg.data, 
@@ -141,31 +252,90 @@ if __name__ == '__main__':
                                 resolution = (resolution_z,resolution_y,resolution_x),
                                 min_distance=min_distance)
         gt = ground_truth.compute_gt()
+
+        # Return gt data as Napari label layer.
         return (gt, {'name': 'gt'}, 'labels')
+    
+    @magicgui(call_button='Load')
+    def load_raw(raw_data_path = pathlib.Path('path/to/raw.tif')) -> napari.types.LayerDataTuple:
+        """
+            Widget to allow the user to load in the raw data that is to be labelled. The data
+            must be a TIF file, and the path to this file provided. This can either be entered
+            manually, or using the dictionary navigation button.
+
+            Clicking the 'Load' call button will load the provided TIF file into a Napari image 
+            layer with name 'raw'.
+        """
+
+        raw_data = np.array([skimage.io.imread(raw_data_path)]) 
+        # Check for stacks of images
+        if (raw_data.shape[0] == 1):
+            raw_data = raw_data[0,:]
+
+        if 'raw' in viewer.layers:
+            return (raw_data, {'name': 'raw'}, 'image')
+        
+        else: 
+            raw = viewer.add_image(data = raw_data, name='raw')
     
     @magicgui(call_button='Load')
     def load_labels(pos_data_path = pathlib.Path('path/to/pos.tif'), 
                     neg_data_path = pathlib.Path('path/to/neg.tif')):
         
+        """
+            A widget to allow the user to load saved pos and neg label layers. Both the 
+            pos and neg files should be TIF files, and the paths can be provided either
+            manually or using dictionary navigation buttons. 
+
+            Clicking the 'Load' button will load the provided TIF files into Napari as
+            the pos and/or neg label layers. If these already exist, they will be overwritten.
+        """
+        
         try:
             pos_data = skimage.io.imread(pos_data_path)
-            pos.data = pos_data 
+            # Check to see if pos label layer already exists
+            if 'pos' in viewer.layers:
+                pos = viewer.layers['pos']
+                pos.data = pos_data 
+            else:
+                pos = viewer.add_labels(data=pos_data, name='pos')
         except:
             if str(pos_data_path) == 'path/to/pos.tif' or str(pos_data_path) == '':
+                # If the path is unaltered or left blank, do nothing. Safetly to not accidently 
+                # overwrite pos layer if only want to load neg layer.
                 pass
             else:
                 raise FileExistsError('Pos data location provided not suitable. Please try again.')
         try:
             neg_data = skimage.io.imread(neg_data_path)
-            neg.data = neg_data 
+            # Check to see if neg label layer already exists
+            if 'neg' in viewer.layers:
+                neg = viewer.layers['neg']
+                neg.data = neg_data 
+            else: 
+                neg = viewer.add_labels(data=neg_data, name='neg')
         except:
             if str(neg_data_path) == 'path/to/neg.tif' or str(neg_data_path) == '':
+                # If the path is unaltered or left blank, do nothing. Safetly to not accidently 
+                # overwrite neg layer if only want to load pos layer.
                 pass
             else:
                 raise FileExistsError('Neg data location provided not suitable. Please try again.')
     
-    @magicgui(call_button='Save centres')
+    @magicgui(save_location ={'mode': 'd'}, call_button='Save centres')
     def save_centres(save_location = pathlib.Path('save/location')):
+        """
+            Widget to allow user to save their pos and neg label layers as TIF files. The 
+            dictionary in which they wish to save their files is provided either manually, 
+            or using the navigation button. 
+
+            Clicking the 'Save centres' button will generate two files, pos.tif and neg.tif, 
+            within the provided directory. Existing files with those names will be overwritten.
+        """
+
+        pos = viewer.layers['pos']
+        neg = viewer.layers['neg']
+
         if not os.path.exists(save_location):
             os.makedirs(save_location)
 
@@ -178,18 +348,33 @@ if __name__ == '__main__':
     @magicgui(call_button='Clear centres')
     def clear_centres(Pos: bool = False, 
                       Neg: bool = False):
+        """
+            A widget to allow the user to generate a clean pos and/or neg label layer. Only
+            layers that have the corresponding tick box checked will be affected. This widget 
+            can be used to generate the pos and neg label layers at the start, if previously 
+            saved ones are not to be loaded. 
+        """
+        
+        raw_layer = viewer.layers['raw']
         if Pos == True:
-            pos.data = np.zeros(raw_data.shape, dtype=np.int64)
+            if 'pos' in viewer.layers:
+                pos = viewer.layers['pos']
+                pos.data = np.zeros(raw_layer.data.shape, dtype=np.int64)
+            else:
+                pos = viewer.add_labels(data = np.zeros(raw_layer.data.shape, dtype=np.int64), name='pos')
+
         if Neg == True:
-            neg.data = np.zeros(raw_data.shape, dtype=np.int64)
-    
-            
+            if 'neg' in viewer.layers:
+                neg = viewer.layers['neg']
+                neg.data = np.zeros(raw_layer.data.shape, dtype=np.int64)
+            else: 
+                neg = viewer.add_labels(data = np.zeros(raw_layer.data.shape, dtype=np.int64), name='neg')
+
+    # Add widgets to napari window
     viewer.window.add_dock_widget(get_gt)
+    viewer.window.add_dock_widget(load_raw)
     viewer.window.add_dock_widget(load_labels)
     viewer.window.add_dock_widget(save_centres)
     viewer.window.add_dock_widget(clear_centres)
 
-
     napari.run()
-
-
