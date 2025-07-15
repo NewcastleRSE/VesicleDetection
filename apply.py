c6c52abdf8710916c5c962c03e01501d72b56ad7
Carmelo Calafiore <dr.carmelo.calafiore@gmail.com>
2025-07-16 00:43:51 +0100

    Update apply.py

--- apply.py
+++ apply.py
@@ -11,13 +11,13 @@ from src.processing.predict import Prediction
 from src.model.model import DetectionModel
 from src.processing.post_processing.hough_detector import HoughDetector
 from src.directory_organisor import create_unique_directory_file
-from src.visualisation import imshow_napari_prediction
+from src.visualisation import show_prediction
 
 from config.load_configs import TRAINING_CONFIG
 
 
 class Apply:
-    def __init__(self, model_checkpoint, label_background=0):
+    def __init__(self, model_checkpoint, label_background=0, n_label_classes=None):
         """
 
         :param model_checkpoint: Path to the model that should be used for prediction.
@@ -34,6 +34,35 @@ class Apply:
             self.label_background = label_background
         else:
             raise TypeError("label_background must be an int or None")
+
+        self._are_label_classes_not_initiated = True
+
+        self.n_label_classes = None
+        self.label_classes = None
+        self.label_classes_no_bg = None
+        self.n_label_classes_no_bg = None
+
+        self.init_label_classes(n_label_classes=n_label_classes)
+
+    def init_label_classes(self, n_label_classes):
+
+        if self._are_label_classes_not_initiated:
+
+            if n_label_classes is None:
+                pass
+            elif isinstance(n_label_classes, int):
+                self.n_label_classes = n_label_classes
+                self.label_classes = [l for l in range(0, self.n_label_classes, 1)]
+
+                self.label_classes_no_bg = [l for l in range(0, self.n_label_classes, 1) if l != self.label_background]
+
+                self.n_label_classes_no_bg = len(self.label_classes_no_bg)
+
+                self._are_label_classes_not_initiated = False
+            else:
+                raise TypeError('n_label_classes must be None or an int')
+
+        return None
 
     def __call__(self, *args, **kwargs):
 
@@ -227,7 +256,7 @@ class Apply:
                 skimage.io.imsave(tiff_file_names_of_different_labels[l], hough_pred_l)
 
         if show:
-            imshow_napari_prediction(zarr_path, save_path)
+            show_prediction(data.numpy(), hough_pred)
 
         return candidates
 
@@ -314,6 +343,302 @@ class Apply:
 
         return candidates
 
+    def predict_probs(self, zarr_path):
+
+
+        data = EMData(zarr_path, 'predict', clahe=TRAINING_CONFIG.clahe)
+
+
+        # Check if there are multiple channels within the raw data.
+        # This shouldn't be the case for us as EM data is 'colourblind'.
+        if len(data.raw_data.shape) == 3:
+            raw_channels = 1
+        elif len(data.raw_data.shape) == 4:
+            raw_channels = data.raw_data.shape[0]
+
+        # Get an instance of the model
+        detection_model = DetectionModel(
+            raw_num_channels = raw_channels,
+            voxel_size = data.voxel_size)
+
+        # Initiate a prediction
+        predictor = Prediction(
+            data = data,
+            model = detection_model,
+            input_shape = TRAINING_CONFIG.input_shape,
+            checkpoint = self.model_checkpoint)
+
+        # Display the border of the output predicition compared to input shape
+        #predictor.print_border_message()
+
+        # Get probabilities
+        ret = predictor.predict_pipeline()
+        probs = torch.nn.Softmax(dim=0)(torch.tensor(ret['prediction'].data)).detach().numpy()
+
+        if self._are_label_classes_not_initiated:
+            self.init_label_classes(n_label_classes=probs.shape[0])
+
+        return probs
+
+    def hough_detection(self, probs, voxel_size, bias=1.0, dtype_labels=None):
+
+        """Use a pretrained vesicle detection model to predict vesicles in unlabelled data by using a single bias.
+
+        :param probs: An Array containing the model-predicted probability of the labels.
+        :type probs: np.ndarray | torch.Tensor
+
+        :param voxel_size: The voxel size of the image.
+        :type voxel_size: int | float
+
+        :param bias: A factor biasing the labelling of vesicle candidates. It labels a candidate as PC- if
+          maxima_pos is less than bias * maxima_neg. Otherwise, it labels it as PC+. So, a bias greater than 1 favours
+          PC- labelling while a bias less than 1 favours PC+.
+        :type bias: int | float | None
+
+        :param dtype_labels: Optional numpy data type of the predicted labels. If it is None (Default), numpy will
+          decide it (usually int64).
+        :type dtype_labels: None | str | np.dtype
+
+        """
+
+        if isinstance(probs, np.ndarray):
+            pass
+        elif isinstance(probs, torch.Tensor):
+            probs = probs.detach().numpy()
+        else:
+            raise TypeError("probs must be a numpy array or torch.Tensor")
+
+        if isinstance(voxel_size, (int, float)):
+            pass
+        else:
+            raise TypeError("voxel_size must be a float or int")
+
+        if bias is None:
+            bias = 1.0
+        elif isinstance(bias, int):
+            bias = float(bias)
+        elif isinstance(bias, float):
+            pass
+        else:
+            raise TypeError("bias must be an int, a float or None")
+
+        pos_pred_data = probs[1, :, :, :]
+        neg_pred_data = probs[2, :, :, :]
+
+        # Post process with hough detector
+        hough_detection = HoughDetector(
+            pred_pos=pos_pred_data,
+            pred_neg=neg_pred_data,
+            voxel_size=voxel_size,
+            bias=bias)
+
+        hough_detection.process()
+        hough_pred = hough_detection.prediction_result
+
+        if dtype_labels is not None:
+            hough_pred = hough_pred.astype(dtype=dtype_labels)
+
+        # candidates = None
+        candidates = hough_detection.accepted_candidates
+
+        return hough_pred, candidates
+
+    def save_predicted_labels_as_zarr(self, labels, zarr_path, raw_data_attrs, bias=None):
+
+        """Use a pretrained vesicle detection model to predict vesicles in unlabelled data by using a single bias.
+
+        :param zarr_path: Path to the zarr group that contains the 'predict' zarr group within it. This path will be fed
+          into the EMData class.
+        :type zarr_path: str
+
+        :param raw_data_attrs: The attributes of the zarr container. They are stored in data.raw_data.attrs.
+
+        :param labels: An Array containing the labels of an image.
+        :type labels: np.ndarray | torch.Tensor
+
+        :param bias: The bias used in the hough detection.
+        :type bias: int | float | None
+
+        """
+
+        if isinstance(labels, np.ndarray):
+            pass
+        elif isinstance(labels, torch.Tensor):
+            labels = labels.detach().numpy()
+        else:
+            raise TypeError("labels must be a numpy array or torch.Tensor")
+
+        date = datetime.today().strftime('%Y-%m-%d')
+
+        # Create save location
+
+        save_path = os.path.join(zarr_path, 'predict', 'Predictions')
+
+        if bias is None:
+            save_path = os.path.join(save_path, 'date_{date:s}'.format(date=date))
+        elif isinstance(bias, int):
+            save_path = os.path.join(save_path, 'bias_{bias:0.3f}_date_{date:s}'.format(bias=bias, date=date))
+        else:
+            raise TypeError('bias must be an int or None')
+
+        save_path = create_unique_directory_file(save_path)
+
+        save_location = os.path.relpath(save_path, zarr_path + '/predict')
+
+        # Save the validation prediction in zarr dictionary.
+        f = zarr.open(zarr_path + '/predict', mode='r+')
+        f[save_location + '/Hough_transformed'] = labels
+
+        for atr in raw_data_attrs:
+            f[save_location + '/Hough_transformed'].attrs[atr] = raw_data_attrs[atr]
+
+        return None
+
+    def save_image_in_1_file(self, image, file_name):
+
+        """Use a pretrained vesicle detection model to predict vesicles in unlabelled data by using a single bias.
+
+        :param image: An Image-like Array.
+        :type image: np.ndarray | torch.Tensor
+
+        :param file_name: The name of the tiff file.
+        :type file_name: str | None
+        """
+
+        if isinstance(image, np.ndarray):
+            pass
+        elif isinstance(image, torch.Tensor):
+            image = image.detach().numpy()
+        else:
+            raise TypeError("image must be a numpy array or torch.Tensor")
+
+        if not isinstance(file_name, str):
+            raise TypeError('file_name must be a string')
+
+        dirname_tiff = os.path.dirname(file_name)
+        if len(dirname_tiff) > 0:
+            os.makedirs(dirname_tiff, exist_ok=True)
+
+        skimage.io.imsave(file_name, image)
+
+        return None
+
+    def save_all_labels_in_1_file(self, labels, file_name=None, bias=None):
+
+        """Use a pretrained vesicle detection model to predict vesicles in unlabelled data by using a single bias.
+
+        :param labels: An Array containing the labels of an image.
+        :type labels: np.ndarray | torch.Tensor
+
+        :param file_name: The names of the files.
+        :type file_name: str | list[str] | tuple[str] | None
+
+        :param bias: The bias used in the hough detection.
+        :type bias: int | float | None
+        """
+
+        if isinstance(labels, np.ndarray):
+            pass
+        elif isinstance(labels, torch.Tensor):
+            labels = labels.detach().numpy()
+        else:
+            raise TypeError("labels must be a numpy array or torch.Tensor")
+
+        if file_name is None:
+            dirname = os.path.join('predicted_labels', 'tiffs')
+            if bias is None:
+                pass
+            elif isinstance(bias, int):
+                dirname = os.path.join(dirname, 'bias_{bias:0.3f}'.format(bias=bias))
+            else:
+                raise TypeError('bias must be an int or None')
+
+            file_name = os.path.join(dirname, 'labels_all.tif')
+
+        elif isinstance(file_name, str):
+            dirname = os.path.dirname(file_name)
+        else:
+            raise TypeError('file_name must be a string or None')
+
+        if len(dirname) > 0:
+            os.makedirs(dirname, exist_ok=True)
+
+        skimage.io.imsave(file_name, labels)
+
+        return None
+
+    def save_different_labels_in_different_files(self, labels, file_names=None, bias=None):
+
+        """Use a pretrained vesicle detection model to predict vesicles in unlabelled data by using a single bias.
+
+        :param labels: An Array containing the labels of an image.
+        :type labels: np.ndarray | torch.Tensor
+
+        :param file_names: The names of the files.
+        :type file_names: str | list[str] | tuple[str] | None
+
+        :param bias: The bias used in the hough detection.
+        :type bias: int | float | None
+        """
+
+        if isinstance(labels, np.ndarray):
+            pass
+        elif isinstance(labels, torch.Tensor):
+            labels = labels.detach().numpy()
+        else:
+            raise TypeError("labels must be a numpy array or torch.Tensor")
+
+        if file_names is None:
+            n_file_names = None
+        elif isinstance(file_names, str):
+            file_names = [file_names]
+            n_file_names = 1
+        elif isinstance(file_names, (list, tuple)):
+            n_file_names = len(file_names)
+            for i in range(0, n_file_names, 1):
+                if not isinstance(file_names[i], str):
+                    raise TypeError('Each Element of file_names must be a string')
+        else:
+            raise TypeError('file_names must be a None, string, list or tuple')
+
+
+        # Save a tiff file per label class, excluding the background label
+
+        if file_names is None:
+
+            dirname = os.path.join('predicted_labels', 'tiffs')
+            if bias is None:
+                pass
+            elif isinstance(bias, int):
+                dirname = os.path.join(dirname, 'bias_{bias:0.3f}'.format(bias=bias))
+            else:
+                raise TypeError('bias must be an int or None')
+
+            file_names = [
+                os.path.join(dirname, 'labels_{label:0>3d}.tif'.format(label=label_l))
+                for label_l in self.label_classes_no_bg
+            ]
+            n_file_names = len(file_names)
+        elif n_file_names == self.n_label_classes_no_bg:
+            pass
+        else:
+            raise TypeError(
+                'file_names must have the same number file names as the label classes '
+                'predicted by the model, excluding the background label.')
+
+        for l in range(0, self.n_label_classes_no_bg, 1):
+
+            labels_l = np.full(shape=labels.shape, fill_value=self.label_background, dtype=labels.dtype)
+            labels_l[labels == self.label_classes_no_bg[l]] = self.label_classes_no_bg[l]
+
+            dirname_tiff_l = os.path.dirname(file_names[l])
+            if len(dirname_tiff_l) > 0:
+                os.makedirs(dirname_tiff_l, exist_ok=True)
+
+            skimage.io.imsave(file_names[l], labels_l)
+
+        return None
+
 if __name__ == "__main__":
         
     data_path = input("Provide path to zarr container: ")
@@ -333,7 +658,7 @@ if __name__ == "__main__":
     else:
         show = visualise.lower() == 'y'
 
-    biases = [1, 1.25, 1.5, 1.75, 2, 2.5, 3, 4, 5]
+    biases = [5]
 
     print("-----")
 
